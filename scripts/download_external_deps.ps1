0caf210d4ca092ce782e45dea9d68b844e7f9afb
Cristi1324 <cmatiut@cloudbasesolutions.com>
2023-08-22 16:45:39 +0300

    download_external_deps.ps1: fix path

--- scripts/download_external_deps.ps1
+++ scripts/download_external_deps.ps1
@@ -3,9 +3,11 @@ param(
     $cloud
 )
 
+$myPath = $MyInvocation.MyCommand.Path
+$repoPath = Split-Path $path -Parent | Split-Path -Parent
 
 function Main {
-    cd "../${cloud}"
+    cd "${repoPath}/${cloud}"
     & "../bin/mkisofs.exe" -o "../../cloudbase-init-config-drive.iso" -ignore-error -ldots -allow-lowercase -allow-multidot -l -publisher "cbsl" -quiet -J -r -V "config-2" "cloudbase-init-metadata" 2>&1 | %{ "$_" }
     if ($LASTEXITCODE -ne 0) {
         Write-Host "Failed to create cloudbase-init-config-drive.iso"
