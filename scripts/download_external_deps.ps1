92cc8de07b6b4a5cc36353514d27e63c10f0fbd6
Cristi1324 <cristianmatiut@yahoo.com>
2023-08-22 16:14:14 +0300

    download_external_deps.ps1: make param mandatory, update main

--- scripts/download_external_deps.ps1
+++ scripts/download_external_deps.ps1
@@ -1,17 +1,21 @@
 param(
+    [Parameter(Mandatory=$true)]
     $cloud
 )
 
 
 function Main {
-    try { git clone "https://github.com/ader1990/cloudbase-init-test-resources-1" cbs-test-res }catch{}
     cd "../${cloud}"
-    try {
-        & "../bin/mkisofs.exe" -o "../../cloudbase-init-config-drive.iso" -ignore-error -ldots -allow-lowercase -allow-multidot -l -publisher "cbsl" -quiet -J -r -V "config-2" "cloudbase-init-metadata" 2>&1 | %{ "$_" }
-    } catch {}
-    try {
-        & "../bin/mkisofs.exe" -o "../../cloudbase-init-config-drive-nocloud.iso" -ignore-error -ldots -allow-lowercase -allow-multidot -l -publisher "cbsl" -quiet -J -r -V "cidata" "cloudbase-init-metadata" 2>&1 | %{ "$_" }
-    } catch {}
+    & "../bin/mkisofs.exe" -o "../../cloudbase-init-config-drive.iso" -ignore-error -ldots -allow-lowercase -allow-multidot -l -publisher "cbsl" -quiet -J -r -V "config-2" "cloudbase-init-metadata" 2>&1 | %{ "$_" }
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed to create cloudbase-init-config-drive.iso"
+        exit 1
+    }
+    & "../bin/mkisofs.exe" -o "../../cloudbase-init-config-drive-nocloud.iso" -ignore-error -ldots -allow-lowercase -allow-multidot -l -publisher "cbsl" -quiet -J -r -V "cidata" "cloudbase-init-metadata" 2>&1 | %{ "$_" }
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed to create cloudbase-init-config-drive-nocloud.iso"
+        exit 1
+    }
 }
 
 Main
